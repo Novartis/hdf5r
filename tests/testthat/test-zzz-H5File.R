f1c73cd8cea0f8fd9dbe6deb381caf9d5beedb70
Holger Hoefling <hhoeflin@gmail.com>
2017-06-11 12:52:37 +0200

    Fixing errors for filename comparison
    Inserted normalizePath when testing filenames to ensure
    that they are appropriately normalized (i.e. \\ on windowns etc)

--- tests/testthat/test-zzz-H5File.R
+++ tests/testthat/test-zzz-H5File.R
@@ -4,19 +4,19 @@ fname <- tempfile(fileext=".h5")
 test_that("H5File-param",{
 	f <- function() file <- h5file(1, "a")
 	expect_that(f(), throws_error("is.character\\(name\\) is not TRUE"))
-	
+
 	f <- function() file <- h5file(c("a", "a"), "a")
 	expect_that(f(), throws_error("length\\(name\\) == 1 is not TRUE"))
-	
+
 	f <- function() file <- h5file("a", 1)
   expect_that(f(), throws_error("'arg' must be NULL or a character vector"))
-	
+
 	f <- function() file <- h5file("a", c("a", "a"))
 	expect_that(f(), throws_error("'arg' must be of length 1"))
-	
+
 	f <- function() file <- h5file("a", c("a", "a"))
   expect_that(f(), throws_error("'arg' must be of length 1"))
-	
+
 	f <- function() file <- h5file("path", "w--")
 	expect_that(f(), throws_error("'arg' should be one of.*"))
 })
@@ -27,12 +27,12 @@ test_that("H5File-FileMode-param-a",{
 	expect_that(file, is_a("H5File"))
 	# TODO: add mode to file
 	#expect_that(file$mode, is_identical_to("a"))
-	expect_that(file$get_file_name(), is_identical_to(fname))
+	expect_that(normalizePath(file$get_file_name(), mustWork=FALSE), is_identical_to(normalizePath(fname, mustWork=FALSE)))
 	group1 <- createGroup(file, "testgroup")
 	expect_that(group1, is_a("H5Group"))
 	h5close(group1)
 	h5close(file)
-	
+
 	# Open existing file for append
 	expect_that(file.exists(fname), is_true())
 	file <- h5file(fname, "a")
@@ -48,10 +48,10 @@ test_that("H5File-FileMode-param-w-",{
 	f <- function() file <- h5file(fname, "w-")
 	expect_that(f(), throws_error("Unable to open file"))
 	expect_that(file.remove(fname), is_true())
-	
+
 	file <- h5file(fname, "w-")
 	expect_that(file$mode, is_identical_to("w-"))
-	expect_that(file$filename, is_identical_to(fname))
+	expect_that(normalizePath(file$filename, mustWork=FALSE), is_identical_to(normalizePath(fname, mustWork=FALSE)))
 	group1 <- createGroup(file, "testgroup1")
 	expect_that(existsGroup(file, "testgroup1"), is_true())
 	h5close(group1)
@@ -60,47 +60,46 @@ test_that("H5File-FileMode-param-w-",{
 
 test_that("H5File-FileMode-param-w",{
   expect_that(file.exists(fname), is_true())
-  # Seems HDF5 since 1.8.15 does not detect that file does exist but is already 
+  # Seems HDF5 since 1.8.15 does not detect that file does exist but is already
   # closed, see also https://www.hdfgroup.org/HDF5/doc/RM/RM_H5F.html
   # We therefore need to delete file
   expect_that(file.remove(fname), is_true())
-      
+
   file <- h5file(fname, "w")
   expect_that(file$mode, is_identical_to("w"))
-  expect_that(file$filename, is_identical_to(fname))
+  expect_that(normalizePath(file$filename, mustWork=FALSE), is_identical_to(normalizePath(fname, mustWork=FALSE)))
   expect_that(existsGroup(file, "testgroup1"), is_false())
   group2 <- createGroup(file, "testgroup1")
   h5close(group2)
   h5close(file)
-	
+
   expect_that(file.exists(fname), is_true())
   # See above
   expect_that(file.remove(fname), is_true())
-  
+
   file <- h5file(fname, "w")
   expect_that(file$mode, is_identical_to("w"))
-  expect_that(file$filename, is_identical_to(fname))
+  expect_that(normalizePath(file$filename, mustWork=FALSE), is_identical_to(normalizePath(fname, mustWork=FALSE)))
   expect_that(existsGroup(file, "testgroup1"), is_false())
   h5close(file)
-  
+
 })
-			
+
 test_that("H5File-FileMode-param-r",{
 	expect_that(file.exists(fname), is_true())
   file <- h5file(fname, "r")
   expect_that(file$mode, is_identical_to("r"))
-  
-  # TODO(mario): Check why for osx we have 
+
+  # TODO(mario): Check why for osx we have
   # > file$filename
   # [1] "/private/var/folders/q3/0dypvmy96jv26m3qk49pp85r0000gn/T/Rtmpl0x2xf/fileb2287a7a7b98.h5"
   # > fname
   # [1] "/var/folders/q3/0dypvmy96jv26m3qk49pp85r0000gn/T//Rtmpl0x2xf/fileb2287a7a7b98.h5"
-  
-  # expect_that(file$filename, is_identical_to(fname))
-	#expect_that(existsGroup(file, "testgroup1"), is_true())
-	h5close(file)
-  
-  expect_that(file.remove(fname), is_true())		
+
+  expect_that(normalizePath(file$filename, mustWork=FALSE), is_identical_to(normalizePath(fname, mustWork=FALSE)))
+  h5close(file)
+
+  expect_that(file.remove(fname), is_true())
   f <- function() file <- h5file(fname, "r")
   expect_that(f(), throws_error("Unable to open file"))
 })
@@ -109,13 +108,13 @@ test_that("H5File-FileMode-param-r+",{
   if(file.exists(fname)) file.remove(fname)
   f <- function() file <- h5file(fname, "r+")
   expect_that(f(), throws_error("Unable to open file"))
-  
+
   file <- h5file(fname, "a")
   group1 <- createGroup(file, "testgroup")
   expect_that(group1, is_a("H5Group"))
   h5close(group1)
   h5close(file)
-  
+
   file <- h5file(fname, "r+")
   expect_that(existsGroup(file, "testgroup"), is_true())
   expect_that(group1, is_a("H5Group"))
@@ -144,7 +143,7 @@ test_that("H5File-is-h5file",{
   group1 <- createGroup(file, "testgroup")
   h5close(group1)
   h5close(file)
-  
+
   expect_that(is.h5file(fname), is_true())
   expect_warning(expect_that(is.h5file("abc"), is_false()))
   fnametxt <- "test.txt"
