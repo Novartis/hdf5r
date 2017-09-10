f2406fdf51528046b64bffc7df5392738ae4a132
mario <mario.annau@gmail.com>
2017-09-10 20:11:54 +0200

    Simplify travis_setup

--- travis_setup.sh
+++ travis_setup.sh
@@ -6,11 +6,7 @@ if [ "$TRAVIS_OS_NAME" == "osx" ]; then # use homebrew version
   echo "brew install finished"
 else # install from source
   cd ..
-  if [ "${HDF5_VERSION:0:4}" == "1.10" ]; then
-    wget "$HDF5_RELEASE_URL/hdf5-1.10/hdf5-1.10.1/src/hdf5-1.10.1.tar.gz"
-  else
-    wget "$HDF5_RELEASE_URL/hdf5-1.8/hdf5-$HDF5_VERSION/src/hdf5-$HDF5_VERSION.tar.gz"
-  fi
+  wget "$HDF5_RELEASE_URL/hdf5-${HDF5_VERSION%.*}/hdf5-$HDF5_VERSION/src/hdf5-$HDF5_VERSION.tar.gz"
   tar -xzf "hdf5-$HDF5_VERSION.tar.gz"
   cd "hdf5-$HDF5_VERSION"
   ./configure --prefix=/usr/local
