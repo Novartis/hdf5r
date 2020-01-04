dd8e8233f62b89c63807bdcbaaf70f52c4de0584
hhoeflin <hhoeflin@gmail.com>
2020-01-04 09:56:47 +0100

    Issues/138 upgrade build env (#139)
    * For issue #138 upgrade build environment to bionic
    
    * only keep the last 200 lines of the hdf5-install

--- travis_setup.sh
+++ travis_setup.sh
@@ -10,6 +10,6 @@ else # install from source
   tar -xzf "hdf5-$HDF5_VERSION.tar.gz"
   cd "hdf5-$HDF5_VERSION"
   ./configure --prefix=/usr/local
-  sudo make install
+  sudo make install 2>&1 | tail -n200
   cd ../hdf5r
 fi
